64536a1e15ea2f142e4040bd27af6ebc240cc1cb
Kyle <kyle.rockman@mac.com>
2013-06-06 13:23:17 -0500

    Perforce Client Bug fixes
    Perforce - Client bug fixes when p4 settings arn't setup correctly, now
    the client just asks for your credentials and you can store them locally
    GUI - Added a P4LoginWidget

--- DTL/perforce/client.py
+++ DTL/perforce/client.py
@@ -4,6 +4,7 @@ from functools import partial
 from P4 import P4, P4Exception
 
 from DTL.api import Path, Logger, apiUtils
+from DTL.gui.widgets import P4LoginWidget
 
 from functools import wraps  # use this to preserve function signatures and docstrings
 
@@ -21,16 +22,21 @@ def requiresP4Conn(func):
 #------------------------------------------------------------
 class P4Client(object):
     #------------------------------------------------------------
-    def __init__(self, clientName, verbose=False):
+    def __init__(self, verbose=False):
         apiUtils.synthesize(self, 'p4Conn', None)
-        apiUtils.synthesize(self, 'p4Client', clientName)
         apiUtils.synthesize(self, 'p4Info', {})
         apiUtils.synthesize(self, 'verbose', verbose)
             
     #------------------------------------------------------------
     def _setupConnection(self):
+        success, user, password, port, client = P4LoginWidget.getCredentials(loginMsg='P4 Login', credentialsFile=apiUtils.getTempFilepath('p4_login.dat'))
+        if not success:
+            raise ValueError('Invalid Login Infomation!')
         p4conn = P4()
-        p4conn.client = self.p4Client()
+        p4conn.port = port
+        p4conn.user = user
+        p4conn.password = password
+        p4conn.client = client
         p4conn.connect()
         p4conn.prog = "DTL Perforce Python Tools"
         p4conn.exception_level = 1 # ignore warnings
@@ -62,6 +68,11 @@ class P4Client(object):
     @requiresP4Conn
     def run(self, *args, **kwds):
         return partial(self.p4Conn().run, *args, **kwds)()
+    
+    #------------------------------------------------------------
+    @requiresP4Conn
+    def test_connection(self):
+        return self.p4Info()
 
     #------------------------------------------------------------
     @requiresP4Conn
@@ -74,6 +85,14 @@ class P4Client(object):
     def getChangeDescription(self, cl):
         return self.p4Conn().fetch_changelist(cl)
     
+    #------------------------------------------------------------
+    @requiresP4Conn
+    def makeClientSpec(self, name, root, view):
+        client_spec = self.p4Conn().fetch_client(name)
+        client_spec['Root'] = root
+        client_spec['View'] = view
+        self.p4Conn().save_client(client_spec)
+    
 if __name__ == '__main__':
-    test_client = P4Client('krockman_CI-20125657_3896')
-    print test_client.getChangeDescription(1295)
+    test_client = P4Client()
+    print test_client.test_connection()
