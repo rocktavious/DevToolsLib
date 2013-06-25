13dd6cc97bcde688b8b8ad613988155ac016ad1b
Kyle <kyle.rockman@mac.com>
2013-06-25 15:54:19 -0500

    First Commit to get PyPi Package up
    This cleans up a few things and allows the package to be distributed on
    pypi.

--- DTL/__init__.py
+++ DTL/__init__.py
@@ -5,7 +5,7 @@ import os
 import sys
 import getpass
 
-__version__ = '1.0.4'
+__version__ = '1.0.7'
 __company__ = 'Cloud Imperium'
 __user__ = getpass.getuser()
 __authors__ = ['Kyle Rockman', 'John Crocker']
