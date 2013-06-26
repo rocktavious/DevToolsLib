9d7ba0278ea651b329a9fab4a205148d9ef475e9
Kyle <kyle.rockman@mac.com>
2013-06-26 15:38:31 -0500

    Bug Fix
    Missed p4loginwidget still listed here

--- DTL/gui/widgets/__init__.py
+++ DTL/gui/widgets/__init__.py
@@ -1,7 +1,6 @@
 from colorpickerwidget import ColorPickerButton, ColorPickerWidget
 from pathwidget import PathWidget
 from loginwidget import LoginWidget
-from p4loginwidget import P4LoginWidget
 from progresswidget import ProgressWidget
 from propertieseditor import PropertiesEditor
 from choicewidget import ChoiceWidget
