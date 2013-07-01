2c4038e7b893a161b119e5162abc3ebafbc9c156
Kyle <kyle.rockman@mac.com>
2013-07-01 13:30:44 -0500

    Bumping Version for Deploy to PyPi

--- DTL/__init__.py
+++ DTL/__init__.py
@@ -29,7 +29,7 @@ def cleanup():
     for file_path in local_path.walk('*.pyc') :
         file_path.remove()
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __company__ = 'Cloud Imperium'
 __user__ = getpass.getuser()
 __authors__ = ['Kyle Rockman', 'John Crocker']
