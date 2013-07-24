e2cc30fe8d5b76bde80be8d79ab7e707c3da0909
Kyle <kyle.rockman@mac.com>
2013-07-24 12:27:44 -0500

    Typo

--- DTL/api/path.py
+++ DTL/api/path.py
@@ -57,7 +57,7 @@ class ClassProperty(property):
         return self.fget.__get__(None, owner)()
 
 #------------------------------------------------------------
-def handleRemoveReadOnly(func, path, exe):
+def handleRemoveReadOnly(func, path, exc):
     excvalue = exc[1]
     if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
         os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) #0777
