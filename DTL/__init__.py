f2981f1351b0623ae53fb973d30db405119e1ccc
Kyle <kyle.rockman@mac.com>
2013-07-24 10:33:53 -0500

    Release 1.1.3

--- DTL/__init__.py
+++ DTL/__init__.py
@@ -5,6 +5,8 @@ Dev Tools Library (DTL)
 multiplatform, multiapplication tools api.
 """
 
+#This Version Number needs to be bumped along with a git checking to auto push to PyPi
+#git commit -a -m "Release 0.0.1" ; git push
 __version__ = '1.1.3'
 __authors__ = ['Kyle Rockman']
 __docformat__ = 'restructuredtext en'
